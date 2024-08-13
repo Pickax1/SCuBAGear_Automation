b0214626da246bb1c6014aa415973dfe3e5b4e9c
Pickax1 <127332158+Pickax1@users.noreply.github.com>
2024-08-13 16:12:12 -0700

    Fixed output and added new variables

--- Invoke-AfterARMDeployment.ps1
+++ Invoke-AfterARMDeployment.ps1
@@ -3,25 +3,31 @@
       - https://github.com/Pickax1/SCuBAGear_Automation/tree/main
 
     Steps
-    1. Create Self-Signed certificate on SCuBA Virtual Machine (VM)
+    1. Connect to Azure and setup variables
+    2. Create Self-Signed certificate on SCuBA Virtual Machine (VM)
       - Save the thumbprint, start/end date to variables in the runbook for use authenticating with Microsoft Graph
-    2. Create a service principal and associate the certificate and assign the service principal with the correct permissions in order to run SCuBAGear
+    3. Create a service principal and associate the certificate and assign the service principal with the correct permissions in order to run SCuBAGear
       - Write values to the runbook variables for use with Microsoft Graph
-    3. Add the hybrid worker extension on the VM and to the hybrid worker group
+    4. Add the hybrid worker extension on the VM and to the hybrid worker group
+    5. Power Platform requirements
 
 #>
 
 ## Requires Az.Accounts,
 
+####################################################
+# Step 1 - Making connections and setting variables
+####################################################
+Write-Host "Step 1: Connecting to Azure and setting variables" -ForegroundColor Yellow
 # Connect
-$AZ = Connect-AzAccount
+Connect-AzAccount
 
 # Retrieve all resource groups
-Get-AzResourceGroup | Select-Object ResourceGroupName, Location
-Write-Output "------------------------------"
-$RG = Read-Host "Enter your Resource Group Name that you deployed the ARM template to, if unknown review the list from above:"
+Get-AzResourceGroup | Select-Object ResourceGroupName, Location | Out-Host
 
-Write-Output "Creating Variables for later use"
+$RG = Read-Host "   Enter your Resource Group Name that you deployed the ARM template to, if unknown review the list from above:"
+
+Write-Output "  Creating Variables for later use"
 $SCuBAVM = Get-AzVM -Name SCuBA -ResourceGroupName $RG
 $VMResourceGroup = $SCuBAVM.Id.Split('/')[4]
 $VmId = $SCuBAVM.Id
@@ -29,14 +35,13 @@ $VM_ID = $SCuBAVM.VmId
 $VMName = $SCuBAVM.Name
 $AutoAccountName = (Get-AzAutomationAccount -ResourceGroupName $RG).AutomationAccountName
 $SubscriptionID = (Get-AzSubscription).ID
-#$Org = (Get-AzTenant).DefaultDomain
-#Identify Storage Account Name
+$Org = (Get-AzTenant).DefaultDomain
 $SA = (Get-AzStorageAccount -ResourceGroupName $RG).StorageAccountName
 
 ########################################
-# Step 1 - Create the certificate on VM
+# Step 2 - Create the certificate on VM
 ########################################
-Write-Output "Step 1: Creating certificate on $($VMName) Virtual Machine, this will take around 1-2 minutes to complete `r`n" -ForegroundColor Yellow
+Write-Host "Step 2: Creating certificate on $($VMName) Virtual Machine, this will take around 1-2 minutes to complete `r`n" -ForegroundColor Yellow
 # Script to create Self-Signed Certificate
 $Script = @"
 Try{
@@ -83,19 +88,19 @@ $StartDate = $outputHashTable['StartDate']
 $EndDate = $outputHashTable['EndDate']
 
 ########################################
-# Step 2 - Create the Service Principal
+# Step 3 - Create the Service Principal
 ########################################
-Write-Output "Step 2: Creating Service Principal: SCuBAGearAutomation and loading certificate thumbprint from $($VMName)  `r`n" -ForegroundColor Yellow
+Write-Host "Step 3: Creating Service Principal: SCuBAGearAutomation and loading certificate thumbprint from $($VMName)  `r`n" -ForegroundColor Yellow
 $SP = New-AzADServicePrincipal -DisplayName SCuBAGearAutomation -CertValue $keyValue -EndDate $EndDate -StartDate $StartDate
 $ServicePrincipalID = $SP.ID
 
 # Update Variables used to connect to Microsoft Graph when running SCuBAGear
-Write-Output "Updating Variables on $($AutoAccountName) Automation Account, these are used to connect to Microsoft Graph when running SCuBAGear on $($VMName) VM"
-Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'ClientID' -Value ($SP).AppID -Encrypted $True
-Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'TenantID' -Value ($SP).AppOwnerOrganizationID -Encrypted $True
-Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'CertThumbprint' -Value $Thumbprint -Encrypted $True
-Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'StorageAccountName' -Value $SA -Encrypted $True
-#Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'Org' -Value $Org -Encrypted $True
+Write-Output "  Updating Variables on $($AutoAccountName) Automation Account, these are used to connect to Microsoft Graph when running SCuBAGear on $($VMName) VM"
+Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'ClientID' -Value ($SP).AppID -Encrypted $True | Out-Null
+Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'TenantID' -Value ($SP).AppOwnerOrganizationID -Encrypted $True | Out-Null
+Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'CertThumbprint' -Value $Thumbprint -Encrypted $True | Out-Null
+Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'StorageAccountName' -Value $SA -Encrypted $True | Out-Null
+Set-AzAutomationVariable -AutomationAccountName $AutoAccountName -ResourceGroupName $VMResourceGroup -Name 'Org' -Value $Org -Encrypted $True | Out-Null
 
 # Assign appropriate graph permissions to the service principal and add to global readers
 function Add-GraphApiRoleToSP {
@@ -134,7 +139,7 @@ function Add-GraphApiRoleToSP {
 
         foreach ($roleItem in $graphRoles) {
             if ($roleItem.id -notIn $msiItem.appRoleAssignments.appRoleId) {
-                Write-Host "Adding role ($($roleItem.value)) to identity: $($ApplicationName).." -ForegroundColor Green
+                Write-Host "    Adding role ($($roleItem.value)) to identity: $($ApplicationName).." -ForegroundColor Green
                 $postBody = @{
                     "principalId" = $msiId
                     "resourceId"  = $graphId
@@ -179,7 +184,7 @@ $roles = @(
 )
 
 # Connect MgGraph
-Write-Output "  - Connecting to Microsoft Graph to add Service Principal $($SP.DisplayName) to $($Roles)"
+Write-Output "  Connecting to Microsoft Graph to add Service Principal $($SP.DisplayName) to $($Roles)"
 Connect-MgGraph -Scopes EntitlementManagement.Read.All,EntitlementManagement.ReadWrite.All -NoWelcome
 $token = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
 
@@ -204,9 +209,9 @@ foreach ($AZRole in $AZRoles){
 }
 
 ################################################
-# Step 3 - Add the VM to the Hybrid Worker Group
+# Step 4 - Add the VM to the Hybrid Worker Group
 ################################################
-Write-Output "Step 3: Adding Hybrid Worker Extension on $($VMName) Virtual Machine  `r`n" -ForegroundColor Yellow
+Write-Host "Step 4: Adding Hybrid Worker Extension on $($VMName) Virtual Machine  `r`n" -ForegroundColor Yellow
 # Install hybrid worker extension on VM
 $VMLocation = $SCuBAVM.Location
 
@@ -220,21 +225,23 @@ $Settings = @{
 
 # Install the Hybrid Worker extension
 Try{
-    Write-Output "Installing Hybrid Worker Extension on $($VmName) VM, this will take 2-3 minutes"
+    Write-Output "  Installing Hybrid Worker Extension on $($VmName) VM, this will take 2-3 minutes"
     $VMExtension = Set-AzVMExtension -ResourceGroupName $RG -VMName $vmName -Location $VMlocation `
         -Name "HybridWorkerExtension" -Publisher "Microsoft.Azure.Automation.HybridWorker" `
         -ExtensionType "HybridWorkerForWindows" -TypeHandlerVersion "1.1" -Settings $Settings `
         -EnableAutomaticUpgrade $true
-}Catch{
+
     if ((Get-AzVMExtension -ResourceGroupName $RG -VMName $VMName -Name HybridWorkerExtension).ProvisioningState -eq 'Succeeded' ){
-        Write-Output "Hybrid Worker Extension was successfully installed on $($VMName)" -ForegroundColor Green
+        Write-Output "  Hybrid Worker Extension was successfully installed on $($VMName)" -ForegroundColor Green
     }else{
-        Write-Error "Hybrid Worker Extension failed to install" -ForegroundColor Red
+        Write-Host "   Hybrid Worker Extension failed to install" -ForegroundColor Red
     }
+}Catch{
+    Write-Error $_.Exception
 }
 
 # Add SCuBA to the Hybrid Worker Group
-Write-Output "  - Adding $($VMName) Virtual Machine to be a member of the $($HybridGroupName)"
+Write-Output "  Adding $($VMName) Virtual Machine to be a member of the $($HybridGroupName)"
 $HybridGroupName = (Get-AzAutomationHybridWorkerGroup -ResourceGroupName $RG -AutomationAccountName $AutoAccountName).Name
 $HybridWorkerParams = @{
     Name = $VM_ID
@@ -245,7 +252,7 @@ $HybridWorkerParams = @{
 }
 New-AzAutomationHybridRunbookWorker @HybridWorkerParams
 
-Write-Output "  - Restarting Hybrid Worker Service on $($SCuBAVM.Name) Virtual Machine to jump start hybrid worker connection"
+Write-Output "  Restarting Hybrid Worker Service on $($SCuBAVM.Name) Virtual Machine to jump start hybrid worker connection"
 # Add code to restart the service
 $Script = @"
     Restart-Service -Name HybridWorkerService -Force
@@ -253,18 +260,21 @@ $Script = @"
 Invoke-AzVMRunCommand -ResourceGroupName $VMResourceGroup -VMName $VMName -CommandId 'RunPowerShellScript' -ScriptString $Script
 
 ###########################################################
-# Step 4 - Associate Service Principal with Power Platform
+# Step 5 - Associate Service Principal with Power Platform
 ###########################################################
-Write-Output "Step 4: Performing Power Platform Requirements" -ForegroundColor Yellow
-# https://github.com/cisagov/ScubaGear/blob/main/docs/prerequisites/noninteractive.md#power-platform
-$appId = ($SP).AppID
-$TenantID = ($SP).AppOwnerOrganizationID
+<#
+    Write-Host "Step 5: Performing Power Platform Requirements" -ForegroundColor Yellow
+    # https://github.com/cisagov/ScubaGear/blob/main/docs/prerequisites/noninteractive.md#power-platform
+    Import-Module Microsoft.PowerApps.Administration.PowerShell
+    $appId = ($SP).AppID
+    $TenantID = ($SP).AppOwnerOrganizationID
 
-# Login interactively with a tenant administrator for Power Platform
-$PowerLogon = Add-PowerAppsAccount -Endpoint prod -TenantID $tenantId 
+    # Login interactively with a tenant administrator for Power Platform
+    $PowerLogon = Add-PowerAppsAccount -Endpoint prod -TenantID $tenantId 
 
-# Register a new application, this gives the SPN / client application same permissions as a tenant admin
-$PowerAppSetup = New-PowerAppManagementApp -ApplicationId $appId
+    # Register a new application, this gives the SPN / client application same permissions as a tenant admin
+    $PowerAppSetup = New-PowerAppManagementApp -ApplicationId $appId
+#>
 
 ##########
 # Cleanup

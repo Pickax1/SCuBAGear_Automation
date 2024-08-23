b890fabf63e03b6d1106d63397014301cd91cfa6
Pickax1 <127332158+Pickax1@users.noreply.github.com>
2024-08-23 16:41:27 -0700

    Created functions, updated product array
    -Created function (Start-SCuBA) for executing SCuBAGear
    -Created function (Start-ResourceConnection) for connecting to Azure and Graph
    -Updated products to aad, teams, exo, and SharePoint

--- src/Run_SCuBAGear.ps1
+++ src/Run_SCuBAGear.ps1
@@ -33,9 +33,10 @@ $TenantID = Get-AutomationVariable -Name 'TenantID'
 $CertificateThumbprint = Get-AutomationVariable -Name 'CertThumbprint'
 $StorageAccountName = Get-AutomationVariable -Name 'StorageAccountName'
 $Org = Get-AutomationVariable -Name 'Org'
+$Environment = Get-AutomationVariable -Name 'Environment'
+Write-Output "Environment is set to: $Environment"
 $Date= Get-Date -Format FileDateTime
 $ContainerName = "scuba-$TenantID-$Date".ToLower()
-
 function Invoke-StorageTransfer {
     Try{
         Write-Output "Service Principal Connected to Azure for writing result to Storage Account"
@@ -64,115 +65,108 @@ function Invoke-StorageTransfer {
         Write-Output "Unable to create blob container"
     }
 }
+Function Start-SCuBA {
+    $Products = @("aad","teams","exo","defender")
 
-Try{
-   if((Get-Module -ListAvailable 'SCuBAGear')){
-       Write-Output "Importing SCuBAGear Module...."
-       Import-Module -Name SCuBAGear -Force
-
-       # This will check for depencies and latest versions
-       Write-Output "Initializing SCuBAGear (This can take awhile)...."
-       Initialize-SCuBA -ScubaParentDirectory C:\Users
-
-       Set-Location C:\Users
-
-       # Connect to Azure and Graph using the service principal and certficate thumbprint
-       Write-Output "Connecting to Azure"
-       Connect-Azaccount -ServicePrincipal -CertificateThumbprint $CertificateThumbprint -ApplicationID $ClientID -TenantID $TenantID
-       Write-Output "Connecting to MGGraph...."
-       Connect-MgGraph -CertificateThumbprint $CertificateThumbprint -ClientID $ClientID -TenantID $TenantID
-       
-       If((Get-MgContext).AppName -eq 'SCuBAGearAutomation'){
-           # Only review AAD and don't try to logon interactively to the portal
-           
-           $Products = @("aad","teams")
-
-           Try{
-               if($Products -Contains "teams"){
-               Write-Output "Connecting to Teams"
-               Connect-MicrosoftTeams -Certificate $CertificateThumbprint -TenantId $TenantID -ApplicationId $ClientID
-                }
-           }Catch{
-               Write-Error $_.Exception
-           }
-           
-           Try{
-                if($Products -Contains "exo"){
-                    Write-Output "Connecting to Exchange"
-                    Connect-ExchangeOnline -CertificateThumbprint $Certificatethumbprint -AppId $ClientID -Organization $Org
-                }
-           }Catch{
-               Write-Error $_.Exception
-           }
-
-           Write-Output "Running SCuBAGear Checks...."
-           Invoke-ScuBA -ProductNames $Products -OPAPath C:\Users\.scubagear\tools\ -OutPath C:\Users\ -LogIn $False -CertificateThumbprint $CertificateThumbprint -AppId $ClientID -Organization $Org
-
-           Write-Output "Transferring SCuBAGear results to storage"
-           Invoke-StorageTransfer
-
-           # Disconnect from graph
-           Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
-           Disconnect-MgGraph
-           
-       }else{
-           Write-Error "Graph Context is wrong....(Get-MgContext).AppName"
-       }
-   }else{
-       # Install and import SCuBAGear module if not already installed and loaded
-       Write-Output "Installing SCuBAGear Module...."
-       Install-Module SCuBAGear -Force -Confirm:$False
-       Write-Output "Importing SCuBAGear Module...."
-       Import-Module SCuBAGear -Force
-
-       # This will check for depencies and latest versions
-       Write-Output "Initializing SCuBAGear (This can take awhile)...."
-       Initialize-SCuBA -ScubaParentDirectory C:\Users
-
-       Set-Location C:\Users
-       
-       # Connect to Azure and Graph using the service principal and certficate thumbprint
-       Write-Output "Connecting to Azure"
-       Connect-Azaccount -ServicePrincipal -CertificateThumbprint $CertificateThumbprint -ApplicationID $ClientID -TenantID $TenantID
-       Write-Output "Connecting to MGGraph...."
-       Connect-MgGraph -CertificateThumbprint $CertificateThumbprint -ClientID $ClientID -TenantID $TenantID
-
-        If((Get-MgContext).AppName -eq 'SCuBAGearAutomation'){
-        
-            $Products = @("aad","teams")
-
-            Try{
-                if($Products -Contains "teams"){
-                Write-Output "Connecting to Teams"
-                Connect-MicrosoftTeams -Certificate $CertificateThumbprint -TenantId $TenantID -ApplicationId $ClientID
-                }
-            }Catch{
-                Write-Error $_.Exception
-            }
-            
-            Try{
-                if($Products -Contains "exo"){
-                    Write-Output "Connecting to Exchange"
-                    Connect-ExchangeOnline -CertificateThumbprint $Certificatethumbprint -AppId $ClientID -Organization $Org
-                }
-            }Catch{
-                Write-Error $_.Exception
-            }
-
-            Write-Output "Running SCuBAGear Checks...."
-            Invoke-ScuBA -ProductNames $Products -OPAPath C:\Users\.scubagear\tools\ -OutPath C:\Users\ -LogIn $False -CertificateThumbprint $CertificateThumbprint -AppId $ClientID -Organization $Org
-
-            Write-Output "Transferring SCuBAGear results to storage"
-            Invoke-StorageTransfer
-
-            # Disconnect from graph
-            Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
-            Disconnect-MgGraph
-            
-        }else{
-            Write-Error "Graph Context is wrong....(Get-MgContext).AppName"
+    Try{
+        if($Products -Contains "teams"){
+        Write-Output "Connecting to Teams"
+        Connect-MicrosoftTeams -Certificate $CertificateThumbprint -TenantId $TenantID -ApplicationId $ClientID
         }
-   }
-}Catch{
-   Write-Error -Message $_.Exception
+    }Catch{
+        Write-Error $_.Exception
+    }
+    
+    Try{
+        if($Products -Contains "exo" -or $Products -Contains "defender"){
+            Write-Output "Connecting to Exchange"
+            Connect-ExchangeOnline -CertificateThumbprint $Certificatethumbprint -AppId $ClientID -Organization $Org
+        }
+    }Catch{
+        Write-Error $_.Exception
+    }
+
+    Try{
+    if($Products -Contains "sharepoint"){
+        $OrgSplit = ($org).Split('.')[0]
+        switch ($Environment) {
+            {"commercial" -or "gcc"} {
+                $M365ENV = "https://$($orgSplit).sharepoint.com"
+            }
+            "gcchigh" {
+                $M365ENV = "https://$($orgSplit).sharepoint.us"
+            }
+            "dod" {
+                $M365ENV = "https://$($orgSplit).sharepoint-mil.us"
+            }
+            default {
+                Write-Error -Message "Unsupported or invalid M365Environment argument"
+            }
+        }
+
+        #$TenantAdminURL = $OrgSplit + '-admin.' + (($M365ENV).Split('.')[-2..-1] -join '.')
+        #Write-Output "Connecting to SharePoint"
+        #Connect-PnpOnline -Thumbprint $CertificateThumbprint -ClientID $ClientID -Tenant $Org -URL $TenantAdminURL
+
+        $TenantAdminURL = "https://" + $OrgSplit + '-admin.' + (($M365ENV).Split('.')[-2..-1] -join '.')
+        Write-Output "Connecting to SharePoint"
+        Connect-PnpOnline -Thumbprint $CertificateThumbprint -ClientID $ClientID -Tenant $Org -URL $M365ENV -TenantAdminURL $TenantAdminURL
+
+        }
+    }
+    Catch{
+        Write-Error $_.Exception
+    }
+
+    Write-Output "Running SCuBAGear Checks...."
+    Invoke-ScuBA -ProductNames $Products -OPAPath C:\Users\.scubagear\tools\ -OutPath C:\Users\ -LogIn $False -CertificateThumbprint $CertificateThumbprint -AppId $ClientID -Organization $Org -M365Environment $Environment
+
+    Write-Output "Transferring SCuBAGear results to storage"
+    Invoke-StorageTransfer
+
+    # Disconnect from graph
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+    Disconnect-MgGraph
+}
+Function Start-ResourceConnection {
+    # Connect to Azure and Graph using the service principal and certficate thumbprint
+    Write-Output "Connecting to Azure"
+    Connect-Azaccount -ServicePrincipal -CertificateThumbprint $CertificateThumbprint -ApplicationID $ClientID -TenantID $TenantID
+    Write-Output "Connecting to MGGraph...."
+    Connect-MgGraph -CertificateThumbprint $CertificateThumbprint -ClientID $ClientID -TenantID $TenantID
+}
+
+if((Get-Module -ListAvailable 'SCuBAGear')){
+    Write-Output "Importing SCuBAGear Module...."
+    Import-Module -Name SCuBAGear -Force
+
+    # This will check for depencies and latest versions
+    Write-Output "Initializing SCuBAGear (This can take awhile)...."
+    Initialize-SCuBA -ScubaParentDirectory C:\Users
+
+    Start-ResourceConnection
+    
+    If((Get-MgContext).AppName -eq 'SCuBAGearAutomation'){        
+        Start-SCuBA           
+    }else{
+        Write-Error "Graph Context is wrong....$(Get-MgContext).AppName"
+    }
+}else{
+    # Install and import SCuBAGear module if not already installed and loaded
+    Write-Output "Installing SCuBAGear Module...."
+    Install-Module SCuBAGear -Force -Confirm:$False
+    Write-Output "Importing SCuBAGear Module...."
+    Import-Module SCuBAGear -Force
+
+    # This will check for depencies and latest versions
+    Write-Output "Initializing SCuBAGear (This can take awhile)...."
+    Initialize-SCuBA -ScubaParentDirectory C:\Users
+
+    Start-ResourceConnection
+
+    If((Get-MgContext).AppName -eq 'SCuBAGearAutomation'){
+        Start-SCuBA         
+    }else{
+        Write-Error "Graph Context is wrong....$(Get-MgContext).AppName"
+    }
 }

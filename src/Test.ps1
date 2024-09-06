60fe91e3c45cd6a700d8f4a7c5779af55da02dd5
Pickax1 <127332158+Pickax1@users.noreply.github.com>
2024-09-06 15:19:39 -0700

    Update Test.ps1

--- src/Test.ps1
+++ src/Test.ps1
@@ -91,13 +91,27 @@ Function Start-ResourceConnection {
     Connect-Azaccount -ServicePrincipal -CertificateThumbprint $CertificateThumbprint -ApplicationID $ClientID -TenantID $TenantID
 }
 
-Write-Output "Importing SCuBAGear Module...."
-$modulePath = "C:\ScubaGear-main\PowerShell\ScubaGear\ScubaGear.psd1"
-Import-Module -Name $modulePath
+if((Get-Module -ListAvailable 'SCuBAGear')){
+    Write-Output "Importing SCuBAGear Module...."
+    Import-Module -Name SCuBAGear -Force
 
-# This will check for depencies and latest versions
-Write-Output "Initializing SCuBAGear (This can take awhile)...."
-Initialize-SCuBA -ScubaParentDirectory C:\
+    # This will check for depencies and latest versions
+    Write-Output "Initializing SCuBAGear (This can take awhile)...."
+    Initialize-SCuBA -ScubaParentDirectory C:\
 
-Start-ResourceConnection  
-Start-SCuBA
+    Start-ResourceConnection  
+    Start-SCuBA           
+}else{
+    # Install and import SCuBAGear module if not already installed and loaded
+    Write-Output "Installing SCuBAGear Module...."
+    Install-Module SCuBAGear -Force -Confirm:$False
+    Write-Output "Importing SCuBAGear Module...."
+    Import-Module SCuBAGear -Force
+
+    # This will check for depencies and latest versions
+    Write-Output "Initializing SCuBAGear (This can take awhile)...."
+    Initialize-SCuBA -ScubaParentDirectory C:\
+
+    Start-ResourceConnection
+    Start-SCuBA
+}
